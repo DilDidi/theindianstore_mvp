940cce3c453570fbe4ebd6db2feb93566e5523a8
DilDidi <indianstorestore@gmail.com>
2026-08-08 00:32:04 +0500

    Updated connection front and back

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// Для Vite используем import.meta.env.PROD
+const baseURL = import.meta.env.PROD
+  ? 'https://theindianstore-backend.onrender.com/api/' // Ссылка на ваш бэкенд на Render
+  : 'http://127.0.0.1:8000/api/';                     // Локально для разработки
+
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: baseURL,
 });
 
 export default API;
