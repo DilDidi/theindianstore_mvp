4ccc781de11364e8255f97dcabef0e0cee433704
DilDidi <indianstorestore@gmail.com>
2026-08-08 02:08:32 +0500

    Fix URL paths in main urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/products/', include('products.urls')),
-    path('api/orders/', include('orders.urls')),
-    path('api/products/', include('products.urls')),
+    path('api/', include('products.urls')),
+    path('api/', include('orders.urls')),    
 ]
 
 if settings.DEBUG:
