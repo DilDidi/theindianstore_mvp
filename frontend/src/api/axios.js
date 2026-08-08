74c2554e622643713391c13fea8c9a0cfe583ca4
DilDidi <indianstorestore@gmail.com>
2026-08-08 23:34:25 +0500

    Fix production backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.PROD
-  ? "https://theindianstore-backend.onrender.com/api/"
-  : "http://127.0.0.1:8000/api/";
-
 const API = axios.create({
-  baseURL,
+  baseURL: import.meta.env.PROD
+    ? "https://theindianstore.onrender.com/api/"
+    : "http://127.0.0.1:8000/api/",
 });
 
 export default API;
