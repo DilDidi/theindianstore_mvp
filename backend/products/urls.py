9930263d0610837bb59a5172d2a0dbe129e57b45
DilDidi <indianstorestore@gmail.com>
2026-08-08 23:22:03 +0500

    Fix product URL routing

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import product_list, product_detail, category_list, banner_list, get_bestsellers
+from .views import (
+    product_list,
+    product_detail,
+    category_list,
+    banner_list,
+    get_bestsellers,
+)
 
 urlpatterns = [
     path('', product_list),
