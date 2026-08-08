b73caea9bd3667d8076a98d668477f116a0a830a
DilDidi <indianstorestore@gmail.com>
2026-08-09 00:26:36 +0500

    Fix static and media files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -184,6 +184,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -256,6 +257,7 @@ CORS_ALLOWED_ORIGINS = [
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
